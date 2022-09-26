8cc3d83ae89c024f888de5a751692da7f5ea9d5b
Joco <szimeth.jozef@gmail.com>
2022-09-26 20:31:31 +0200

    feat: implement pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -1,8 +1,50 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
+#define ERROR -1
+#define PIPE_READ_FD 0
+#define PIPE_WRITE_FD 1
+
 int main(int argc, char* argv[])
 {
-    printf("Running pinpong...\n");
+    int child_direction[2];
+    int parent_direction[2];
+
+    if (pipe(child_direction) != 0)
+    {
+        fprintf(2, "Unexpected error during child_direction pipe initialization.\n");
+        exit(ERROR);
+    }
+
+    if (pipe(parent_direction) != 0)
+    {
+        fprintf(2, "Unexpected error during parent_direction pipe initialization.\n");
+        exit(ERROR);
+    }
+
+    int pid = fork();
+    if (pid < 0)
+    {
+        fprintf(2, "Fork failed.");
+        exit(ERROR);
+    }
+
+    if (pid == 0)
+    {
+        // Child process
+        char buf;
+        read(child_direction[PIPE_READ_FD], &buf, 1);
+        printf("%d: received ping\n", getpid());
+        write(parent_direction[PIPE_WRITE_FD], "b", 1);
+    }
+    else
+    {
+        // Parent process
+        write(child_direction[PIPE_WRITE_FD], "a", 1);
+        char buf;
+        read(parent_direction[PIPE_READ_FD], &buf, 1);
+        printf("%d: received pong\n", getpid());
+    }
+
     exit(0);
 }
--- user/sleep.c
+++ user/sleep.c
@@ -5,18 +5,18 @@ int main(int argc, char* argv[])
 {
     if (argc < 2)
     {
-        printf("Usage: sleep <secodns>\n");
+        printf("Usage: sleep <ticks>\n");
         exit(0);
     }
 
-    int seconds = atoi(argv[1]);
+    int ticks = atoi(argv[1]);
 
-    if (seconds < 1)
+    if (ticks < 1)
     {
-        printf("Invalid interval %s\n", argv[1]);
+        fprintf(2, "Invalid interval %s\n", argv[1]);
         exit(1);
     }
 
-    sleep(seconds);
+    sleep(ticks);
     exit(0);
 }
