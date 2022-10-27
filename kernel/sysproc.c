1c613bf806edaa5c0c5f2dc40d2b904047ecf1fe
Joco <szimeth.jozef@gmail.com>
2022-10-27 21:08:06 +0200

    implement pgaccess syscall

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -48,8 +48,7 @@ sys_sbrk(void)
   return addr;
 }
 
-uint64
-sys_sleep(void)
+uint64 sys_sleep(void)
 {
   int n;
   uint ticks0;
@@ -58,8 +57,10 @@ sys_sleep(void)
   argint(0, &n);
   acquire(&tickslock);
   ticks0 = ticks;
-  while(ticks - ticks0 < n){
-    if(killed(myproc())){
+  while (ticks - ticks0 < n)
+  {
+    if (killed(myproc()))
+    {
       release(&tickslock);
       return -1;
     }
@@ -71,16 +72,68 @@ sys_sleep(void)
 
 
 #ifdef LAB_PGTBL
-int
-sys_pgaccess(void)
+int sys_pgaccess(void)
 {
-  // lab pgtbl: your code here.
-  return 0;
+  uint64 base;
+  uint64 mask;
+  int len;
+
+  pagetable_t pagetable = 0;
+  unsigned int procmask = 0;
+  pte_t* pte;
+
+  struct proc* p = myproc();
+
+  argaddr(0, &base);
+  argint(1, &len);
+  argaddr(2, &mask);
+
+  int maxlen = sizeof(int)*8;
+  if (len > maxlen)
+    len = maxlen;
+
+  for (int i = 0; i < len; i++)
+  {
+    pagetable = p->pagetable;
+
+    if (base >= MAXVA)
+      panic("pgaccess: base is greater or equal MAXVA");
+
+    for (int level = 2; level > 0; level--)
+    {
+      pte = &pagetable[PX(level, base)];
+      if (*pte & PTE_V)
+        pagetable = (pagetable_t)PTE2PA(*pte);
+      else
+        return -1;
+    }
+
+    pte = &pagetable[PX(0, base)];
+    if (pte == 0)
+      return -1;
+
+    if ((*pte & PTE_V) == 0)
+      return -1;
+
+    if ((*pte & PTE_U) == 0)
+      return -1;
+
+    if (*pte & PTE_A)
+    {
+      procmask |= (1L << i);
+      *pte = *pte & (~PTE_A);
+    }
+
+    base += PGSIZE;
+  }
+
+  pagetable = p->pagetable;
+
+  return copyout(pagetable, mask, (char*)&procmask, sizeof(unsigned int));
 }
 #endif
 
-uint64
-sys_kill(void)
+uint64 sys_kill(void)
 {
   int pid;
 
@@ -90,8 +143,7 @@ sys_kill(void)
 
 // return how many clock tick interrupts have occurred
 // since start.
-uint64
-sys_uptime(void)
+uint64 sys_uptime(void)
 {
   uint xticks;
 
