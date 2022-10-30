5678848875403b2daa88177cdd231aa1ea15bf53
Joco <szimeth.jozef@gmail.com>
2022-10-30 21:47:11 +0100

    Clarified xargs program, but still ugly af tho

--- user/xargs.c
+++ user/xargs.c
@@ -7,60 +7,69 @@
 
 #define ERROR -1
 #define SUCCESS 0
-#define STD_INPUT 0  // process reads from file descriptor
+#define STD_INPUT 0
+#define CHILD_PROC_ID 0
 
 int main(int argc, char* argv[])
 {
+    printf("Process id: %d\n", getpid());
     if (argc < 2)
     {
         fprintf(2, "usage: xargs <command> [argv...]\n");
         exit(ERROR);
     }
 
-    int c;
-    int blanks = 0, offset = 0;
-    char* xargs_command[MAXARG];
-    char ch, buf[2048];
-    char* p = buf;
+    int next_arg_index;
+    int next_buf_pos = 0;
+    char* xargs_arguments[MAXARG];
+    char chr;
+    char buf[2048];
+    char* buf_ptr = buf;
 
-    // c = 1, cuz we ignore "xargs" in ["xargs",...]
+    // argv[c + 1], cuz we ignore "xargs" in ["xargs",...]
     // Save command and it's args passed directly to xargs
-    for (c = 1; c < argc; c++)
-        xargs_command[c - 1] = argv[c];
-
-    --c;
+    for (next_arg_index = 0; next_arg_index < argc - 1; next_arg_index++)
+        xargs_arguments[next_arg_index] = argv[next_arg_index + 1];
 
     // Read from std_input byte by byte
-    while (read(STD_INPUT, &ch, 1) > 0)
+    while (read(STD_INPUT, &chr, 1) > 0)
     {
-        if (IS_BLANK(ch))
+        if (IS_BLANK(chr))
         {
-            blanks++;
+            // ch is a blank character, so whatever string is in buf null-terminate it
+            buf[next_buf_pos++] = 0;
+
+            // Save string stored at buf_ptr to xargs_arguments
+            xargs_arguments[next_arg_index++] = buf_ptr;
+
+            // Shift buf_ptr, so it points to the next position in buf for the new/next argument
+            // buf = ['h', 'e', 'l', 'l', 'o', \0, _ , _, ...]
+            //        ^                            ^
+            //        |__previous buf_ptr          |__new buf_ptr
+            buf_ptr = buf + next_buf_pos;
+
+            // Read next byte from STD_INPUT
             continue;
         }
 
-        if (blanks)
-        {
-            buf[offset++] = 0;
-
-            xargs_command[c++] = p;
-            p = buf + offset;
-
-            blanks = 0;
-        }
-
-        if (ch != '\n')
-            buf[offset++] = ch;
+        if (chr != '\n')
+            buf[next_buf_pos++] = chr;
         else
         {
-            xargs_command[c++] = p;
-            p = buf + offset;
+            // At this point chr must a new line character
+            // So save whatever string is at buf_ptr to xargs_arguments
+            xargs_arguments[next_arg_index++] = buf_ptr;
+            // Shift buf_ptr by the current arg length (pipe may contain multiple lines a.k.a. \n)
+            buf_ptr = buf + next_buf_pos;
 
-            if (!fork())
-                exit(exec(xargs_command[0], xargs_command));
+            if (fork() == CHILD_PROC_ID)
+                exit(exec(xargs_arguments[0], xargs_arguments));
 
             wait(0);
-            c = argc - 1;
+
+            // When chr is \n reset xargs_arguments to arguments loaded
+            // from argv i.e. remove previously loade args from STD_INPUT
+            next_arg_index = argc - 1;
         }
     }
 
