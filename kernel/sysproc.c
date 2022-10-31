e85220ffcf1fa293e287d6fbf80748414cbe5be2
Joco <szimeth.jozef@gmail.com>
2022-10-31 14:49:41 +0100

    improved pgaccess syscall

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -72,66 +72,41 @@ uint64 sys_sleep(void)
 
 
 #ifdef LAB_PGTBL
+// int sys_pgaccess(void)
+// {
+//   YOUR CODE...
+// }
+#endif
+
 int sys_pgaccess(void)
 {
-  uint64 base;
-  uint64 mask;
-  int len;
+  uint64 start_va;
+  int pagecount;
+  uint64 user_address;
 
-  pagetable_t pagetable = 0;
-  unsigned int procmask = 0;
-  pte_t* pte;
+  uint64 bitmask = 0;
 
-  struct proc* p = myproc();
+  argaddr(0, &start_va);
+  argint(1, &pagecount);
+  argaddr(2, &user_address);
 
-  argaddr(0, &base);
-  argint(1, &len);
-  argaddr(2, &mask);
-
-  int maxlen = sizeof(int)*8;
-  if (len > maxlen)
-    len = maxlen;
-
-  for (int i = 0; i < len; i++)
+  uint64 va = start_va;
+  for (int i = 0; i < pagecount; i++)
   {
-    pagetable = p->pagetable;
-
-    if (base >= MAXVA)
-      panic("pgaccess: base is greater or equal MAXVA");
-
-    for (int level = 2; level > 0; level--)
-    {
-      pte = &pagetable[PX(level, base)];
-      if (*pte & PTE_V)
-        pagetable = (pagetable_t)PTE2PA(*pte);
-      else
-        return -1;
-    }
-
-    pte = &pagetable[PX(0, base)];
-    if (pte == 0)
-      return -1;
-
-    if ((*pte & PTE_V) == 0)
-      return -1;
-
-    if ((*pte & PTE_U) == 0)
-      return -1;
+    // Get page-table-entry for page where va is located
+    pte_t* pte = walk(myproc()->pagetable, va, 0);
 
     if (*pte & PTE_A)
     {
-      procmask |= (1L << i);
-      *pte = *pte & (~PTE_A);
+      bitmask |= (1 << i);
+      *pte &= ~PTE_A;
     }
 
-    base += PGSIZE;
+    va += PGSIZE;
   }
 
-  pagetable = p->pagetable;
-
-  return copyout(pagetable, mask, (char*)&procmask, sizeof(unsigned int));
+  return copyout(myproc()->pagetable, user_address, (char*)&bitmask, sizeof(uint64));
 }
-#endif
 
 uint64 sys_kill(void)
 {
