27069df3141b639551d254e48db05f70b0105738
Joco <szimeth.jozef@gmail.com>
2022-12-09 23:42:30 +0100

    Implement 'Uthread: prepínanie medzi vláknami (stredná)'

--- user/uthread.c
+++ user/uthread.c
@@ -11,9 +11,29 @@
 #define MAX_THREAD  4
 
 
+struct context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
+
 struct thread {
   char stack[STACK_SIZE]; /* the thread's stack */
   int  state;             /* FREE, RUNNING, RUNNABLE */
+  struct context context;
 };
 
 struct thread all_thread[MAX_THREAD];
@@ -50,7 +70,7 @@ void thread_schedule(void)
       break;
     }
 
-    t = t + 1;
+    t += 1;
   }
 
   if (next_thread == 0) {
@@ -64,10 +84,8 @@ void thread_schedule(void)
     next_thread->state = RUNNING;
     t = current_thread;
     current_thread = next_thread;
-    /* YOUR CODE HERE
-     * Invoke thread_switch to switch from t to next_thread:
-     * thread_switch(??, ??);
-     */
+    // Invoke thread_switch to switch from t to next_thread:
+    thread_switch((uint64)&t->context, (uint64)&current_thread->context);
   }
   else
     next_thread = 0;
@@ -83,7 +101,8 @@ void thread_create(void (*func)())
   }
 
   t->state = RUNNABLE;
-  // YOUR CODE HERE
+  t->context.ra = (uint64)func;
+  t->context.sp = (uint64)&t->stack[STACK_SIZE - 1];
 }
 
 void thread_yield(void)
